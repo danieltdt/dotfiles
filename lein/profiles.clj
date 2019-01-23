79b900fe36e8d6a3d1cfe31efb6dfd88429f8862
Daniel Teixeira <daniel.t.dt@gmail.com>
2019-01-23 10:38:54 -0200

    chore: update nrepl cider & refactor plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,7 @@
-{:user {:plugins [[refactor-nrepl "2.3.1"]
-                  [cider/cider-nrepl "0.15.1"]]
+{:user {:plugins [[refactor-nrepl "2.4.0"]
+                  [cider/cider-nrepl "0.20.0"]]
                   ;;[org.clojure/tools.nrepl "0.2.13"]
                   ;;[clojure-complete "0.2.5"]]
+        :middleware [cider-nrepl.plugin/middleware
+                     refactor-nrepl.plugin/middleware]
         :jvm-opts ["-Xmx2g"]}}
