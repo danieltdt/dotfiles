19210b7348bae3be94dee89cb18d56090d994b0a
Daniel Teixeira <daniel.t.dt@gmail.com>
2019-02-22 14:41:33 -0300

    organize repl profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,7 @@
-{:user {:plugins [[refactor-nrepl "2.4.0"]
-                  [cider/cider-nrepl "0.20.0"]]
+{:user {:jvm-opts ["-Xmx2g"]}
+ :repl {:plugins [[refactor-nrepl "2.4.0"]
+                  [cider/cider-nrepl "0.20.0"]
+                  [mvxcvi/whidbey "2.0.0"]]
         :middleware [cider-nrepl.plugin/middleware
-                     refactor-nrepl.plugin/middleware]
-        :jvm-opts ["-Xmx300m"]}}
+                     refactor-nrepl.plugin/middleware
+                     whidbey.plugin/repl-pprint]}}
