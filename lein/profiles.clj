17aa5542c045d54041ec80a321f57b959eb629dc
Daniel Teixeira <daniel.t.dt@gmail.com>
2019-03-01 15:41:03 -0300

    fix lein repl deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,5 @@
-{:user {:jvm-opts ["-Xmx2g"]}
- :repl {:plugins    [[cider/cider-nrepl "0.20.0"]
-                     [org.clojure/tools.nrepl "0.2.13"]
-                     [refactor-nrepl "2.4.0"]
-                     [mvxcvi/whidbey "2.0.0"]]
-        :middleware [cider-nrepl.plugin/middleware
-                     refactor-nrepl.plugin/middleware
-                     whidbey.plugin/middleware]}}
+{:user {:jvm-opts ["-Xmx2g"]
+        :plugins  [[cider/cider-nrepl "0.21.1"]
+                   [nrepl "0.6.0"]
+                   [refactor-nrepl "2.4.1-SNAPSHOT" :exclusions [nrepl org.clojure/tools.logging]]]}
+ :repl {:plugins [[mvxcvi/whidbey "2.0.0"]]}}
