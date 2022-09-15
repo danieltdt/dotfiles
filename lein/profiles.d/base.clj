61111d2bc27c4525e9f68dc605f2b533e0ae20d1
Daniel Teixeira <daniel.t.dt@gmail.com>
2022-09-15 14:15:00 -0300

    fix: lein

--- lein/profiles.d/base.clj
+++ lein/profiles.d/base.clj
@@ -1,5 +1,6 @@
 {:jvm-opts ["-Xmx2g"]
- :repl {:plugins [[cider/cider-nrepl "0.28.5"]
-                  [refactor-nrepl "2.5.5" :exclusions [nrepl org.clojure/tools.logging]]
-                  [mvxcvi/whidbey "2.2.1"]]
-        :middleware [whidbey.plugin/repl-pprint]}}
+ :plugins [[cider/cider-nrepl "0.28.5" :exclusions [nrepl]]
+           [nrepl "1.0.0"]
+           [refactor-nrepl "3.5.5" :exclusions [nrepl org.clojure/tools.logging]]
+           [mvxcvi/whidbey "2.2.1"]]
+ :middleware [whidbey.plugin/repl-pprint]}
